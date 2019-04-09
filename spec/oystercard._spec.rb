7e9ad0fe46b8f5bf35480ef7fa6faa7a529bb02b
FrankieBoye <frankie4@btinternet.com>
2019-04-09 11:43:51 +0100

    complete challenge #05: enable top up functionality

--- spec/oystercard._spec.rb
+++ spec/oystercard._spec.rb
@@ -1,8 +1,15 @@
 require 'oystercard'
 
 describe Oystercard do
-it "checks the new card has a balance" do
-  expect(subject.balance).to eq (0)
-end
+  it 'checks the new card has a balance' do
+    expect(subject.balance).to eq(0)
+  end
 
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'can top up the balance' do
+      expect{ subject.top_up 1}.to change { subject.balance }.by 1
+    end
+  end  
 end
