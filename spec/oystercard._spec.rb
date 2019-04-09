4a99d0061252f1da5e932bc255ccc4dc930d38c1
FrankieBoye <frankie4@btinternet.com>
2019-04-09 14:20:49 +0100

    complete challenge #07: deduct the money

--- spec/oystercard._spec.rb
+++ spec/oystercard._spec.rb
@@ -8,15 +8,19 @@ describe Oystercard do
   describe '#top_up' do
     it { is_expected.to respond_to(:top_up).with(1).argument }
 
-      it 'can top up the balance' do
-        expect{ subject.top_up 1 }.to change { subject.balance }.by 1
-      end
-
-        it 'raises an error if the maximum balance is exceeded' do
-          maximum_balance = Oystercard::MAXIMUM_BALANCE
-          subject.top_up maximum_balance
-          expect{ subject.top_up 1 }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
-        end
-
-     end
+    it 'can top up the balance' do
+      expect{ subject.top_up 1 }.to change { subject.balance }.by 1
     end
+
+    it 'raises an error if the maximum balance is exceeded' do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      subject.top_up maximum_balance
+      expect{ subject.top_up 1 }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
+    end
+  end
+
+  it 'can deduct the balance' do
+    expect{ subject.deduct 10 }.to change { subject.balance }.by -10
+  end
+
+end
